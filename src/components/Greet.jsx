1eb870ecc8756e8c7826f86578667cbd45a54a43
amontalvof <amontalvof92@gmail.com>
2023-10-12 18:42:02 -0400

    feat: StyleSheet API

--- src/components/Greet.jsx
+++ src/components/Greet.jsx
@@ -1,17 +1,24 @@
-import { Text, View } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
 
 const Greet = ({ name }) => {
     return (
-        <View
-            style={{
-                flex: 1,
-                backgroundColor: 'plum',
-                padding: 60,
-            }}
-        >
-            <Text>Hello {name}</Text>
+        <View style={styles.container}>
+            <Text style={styles.title}>Hello {name}</Text>
         </View>
     );
 };
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'plum',
+        padding: 60,
+    },
+    title: {
+        color: 'white',
+        fontSize: 24,
+        fontWeight: 'bold',
+    },
+});
 
 export default Greet;
