0c3b4e36fe114a81edaf4272af09611238fc1ff8
amontalvof <amontalvof92@gmail.com>
2023-11-13 21:10:55 -0500

    feat: Align Items

--- App.js
+++ App.js
@@ -5,7 +5,9 @@ export default function App() {
     return (
         // <View style={{ backgroundColor: 'plum', flex: 1 }}></View>
         <View style={styles.container}>
-            <Box style={{ backgroundColor: '#8e9b00' }}>Box 1</Box>
+            <Box style={{ backgroundColor: '#8e9b00', paddingVertical: 100 }}>
+                Box 1
+            </Box>
             <Box style={{ backgroundColor: '#b65d1f' }}>Box 2</Box>
             <Box style={{ backgroundColor: '#1c4c56' }}>Box 3</Box>
             {/* <Box style={{ backgroundColor: '#ab9156' }}>Box 4</Box>
@@ -20,7 +22,7 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         flexDirection: 'row',
-        justifyContent: 'center',
+        alignItems: 'baseline',
         marginTop: 64,
         borderWidth: 6,
         borderColor: 'red',
