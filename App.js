104ea110b3d4fc3ad55aa9c418b1184dae1c7a70
amontalvof <amontalvof92@gmail.com>
2023-12-05 20:23:55 -0500

    feat: Dimensions API

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
 
 export default function App() {
     return (
-        // <View style={{ backgroundColor: 'plum', flex: 1 }}></View>
-        <View style={styles.container}></View>
+        <View style={styles.container}>
+            <View style={styles.box}>
+                <Text style={styles.text}>Welcome!</Text>
+            </View>
+        </View>
     );
 }
+
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
 
 const styles = StyleSheet.create({
     container: {
@@ -13,5 +19,15 @@ const styles = StyleSheet.create({
         backgroundColor: 'plum',
         alignItems: 'center',
         justifyContent: 'center',
+    },
+    box: {
+        width: windowWidth > 500 ? '70%' : '90%',
+        height: windowHeight > 600 ? '60%' : '90%',
+        backgroundColor: 'lightblue',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    text: {
+        fontSize: windowWidth > 500 ? 50 : 30,
     },
 });
