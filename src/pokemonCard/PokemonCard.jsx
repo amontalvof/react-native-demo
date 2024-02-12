11d2a43906a377cd2546032aeb4a1d0641a145f0
amontalvof <amontalvof92@gmail.com>
2024-02-11 19:02:58 -0500

    feat: Build a Pokemon Card (2/6)

--- src/pokemonCard/PokemonCard.jsx
+++ src/pokemonCard/PokemonCard.jsx
@@ -1,11 +1,32 @@
-import { Text, View } from 'react-native';
+import { StyleSheet, Text, View, Platform } from 'react-native';
 
 const PokemonCard = () => {
     return (
-        <View>
+        <View style={styles.card}>
             <Text>PokemonCard</Text>
         </View>
     );
 };
+
+const styles = StyleSheet.create({
+    card: {
+        backgroundColor: 'white',
+        borderRadius: 16,
+        borderWidth: 2,
+        padding: 16,
+        margin: 16,
+        ...Platform.select({
+            ios: {
+                shadowOffset: { width: 2, height: 2 },
+                shadowColor: '#333',
+                shadowOpacity: 0.3,
+                shadowRadius: 4,
+            },
+            android: {
+                elevation: 5,
+            },
+        }),
+    },
+});
 
 export default PokemonCard;
