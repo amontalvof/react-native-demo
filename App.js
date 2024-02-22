5b89e16ac3b9c2a4ec7e259b0dee5d40df5db533
amontalvof <amontalvof92@gmail.com>
2024-02-21 21:11:08 -0500

    feat: Lists

--- App.js
+++ App.js
@@ -1,50 +1,23 @@
-import { StyleSheet, SafeAreaView, Platform, ScrollView } from 'react-native';
-import PokemonCard from './src/pokemonCard/PokemonCard';
+import {
+    StyleSheet,
+    View,
+    Text,
+    ScrollView,
+    SafeAreaView,
+    StatusBar,
+} from 'react-native';
+import pokemonList from './src/rnList/data.json';
 
 export default function App() {
-    const charmanderData = {
-        name: 'Charmander',
-        image: require('./assets/charmander.png'),
-        type: 'Fire',
-        hp: 39,
-        moves: ['Scratch', 'Ember', 'Growl', 'Leer'],
-        weaknesses: ['Water', 'Rock'],
-    };
-
-    const squirtleData = {
-        name: 'Squirtle',
-        image: require('./assets/squirtle.png'),
-        type: 'Water',
-        hp: 44,
-        moves: ['Tackle', 'Water Gun', 'Tail Whip', 'Withdraw'],
-        weaknesses: ['Electric', 'Grass'],
-    };
-
-    const bulbasaurData = {
-        name: 'Bulbasaur',
-        image: require('./assets/bulbasaur.png'),
-        type: 'Grass',
-        hp: 45,
-        moves: ['Tackle', 'Vine Whip', 'Growl', 'Leech Seed'],
-        weaknesses: ['Fire', 'Ice', 'Flying', 'Psychic'],
-    };
-
-    const pikachuData = {
-        name: 'Pikachu',
-        image: require('./assets/pikachu.png'),
-        type: 'Electric',
-        hp: 35,
-        moves: ['Quick Attack', 'Thunderbolt', 'Tail Whip', 'Growl'],
-        weaknesses: ['Ground'],
-    };
-
     return (
         <SafeAreaView style={styles.container}>
-            <ScrollView>
-                <PokemonCard {...charmanderData} />
-                <PokemonCard {...squirtleData} />
-                <PokemonCard {...bulbasaurData} />
-                <PokemonCard {...pikachuData} />
+            <ScrollView style={styles.scrollView}>
+                {pokemonList.map((pokemon) => (
+                    <View key={pokemon.id} style={styles.card}>
+                        <Text style={styles.cardText}>{pokemon.type}</Text>
+                        <Text style={styles.cardText}>{pokemon.name}</Text>
+                    </View>
+                ))}
             </ScrollView>
         </SafeAreaView>
     );
@@ -54,6 +27,19 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#f5f5f5',
-        paddingTop: Platform.OS === 'android' ? 50 : 0,
+        paddingTop: StatusBar.currentHeight,
+    },
+    scrollView: {
+        paddingHorizontal: 16,
+    },
+    card: {
+        backgroundColor: 'white',
+        padding: 16,
+        borderRadius: 8,
+        borderWidth: 1,
+        margin: 16,
+    },
+    cardText: {
+        fontSize: 30,
     },
 });
