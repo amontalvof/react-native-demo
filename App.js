f79491cbc4ac97e879515e4ff98050e1df0d3df4
amontalvof <amontalvof92@gmail.com>
2024-03-12 21:30:32 -0400

    Add dark mode switch to App.js

--- App.js
+++ App.js
@@ -5,10 +5,13 @@ import {
     StatusBar,
     TextInput,
     Text,
+    Switch,
+    View,
 } from 'react-native';
 
 export default function App() {
     const [name, setName] = useState('');
+    const [isDarkMode, setIsDarkMode] = useState(false);
     return (
         <SafeAreaView style={styles.container}>
             <TextInput
@@ -27,6 +30,17 @@ export default function App() {
                 multiline
             />
             <Text style={styles.text}>My name is {name}</Text>
+            <View style={styles.switchContainer}>
+                <Text style={styles.text}>Dark Mode</Text>
+                <Switch
+                    value={isDarkMode}
+                    onValueChange={() =>
+                        setIsDarkMode((prevState) => !prevState)
+                    }
+                    trackColor={{ false: '#767577', true: '#81b0ff' }}
+                    thumbColor="#f4f3f4"
+                />
+            </View>
         </SafeAreaView>
     );
 }
@@ -50,5 +64,11 @@ const styles = StyleSheet.create({
     multilineText: {
         minHeight: 100,
         textAlignVertical: 'top',
+    },
+    switchContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        padding: 10,
     },
 });
