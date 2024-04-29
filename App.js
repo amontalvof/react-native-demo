69b3f3ef15e2461669ce4299d41d4a2032de20f0
amontalvof <amontalvof92@gmail.com>
2024-04-29 08:48:39 -0400

    Refactor App.js: Add functionality to add new posts

--- App.js
+++ App.js
@@ -7,12 +7,17 @@ import {
     Text,
     View,
     ActivityIndicator,
+    TextInput,
+    Button,
 } from 'react-native';
 
 export default function App() {
     const [postList, setPostList] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
     const [refreshing, setRefreshing] = useState(false);
+    const [postBody, setPostBody] = useState('');
+    const [postTitle, setPostTitle] = useState('');
+    const [isPosting, setIsPosting] = useState(false);
 
     const fetchData = async (limit = 10) => {
         try {
@@ -33,6 +38,29 @@ export default function App() {
         setRefreshing(false);
     };
 
+    const addPost = async () => {
+        setIsPosting(true);
+        const response = await fetch(
+            'https://jsonplaceholder.typicode.com/posts',
+            {
+                method: 'POST',
+                body: JSON.stringify({
+                    title: postTitle,
+                    body: postBody,
+                    userId: 1,
+                }),
+                headers: {
+                    'Content-type': 'application/json; charset=UTF-8',
+                },
+            }
+        );
+        const newPost = await response.json();
+        setPostList([newPost, ...postList]);
+        setPostTitle('');
+        setPostBody('');
+        setIsPosting(false);
+    };
+
     useEffect(() => {
         fetchData(3);
         return () => {};
@@ -49,45 +77,68 @@ export default function App() {
 
     return (
         <SafeAreaView style={styles.container}>
-            <View style={styles.listContainer}>
-                <FlatList
-                    data={postList}
-                    renderItem={({ item }) => {
-                        return (
-                            <View style={styles.card}>
-                                <Text style={styles.titleText}>
-                                    {item.title}
+            <>
+                <View style={styles.inputContainer}>
+                    <TextInput
+                        style={styles.input}
+                        placeholder="Post Title"
+                        value={postTitle}
+                        onChangeText={setPostTitle}
+                    />
+                    <TextInput
+                        style={styles.input}
+                        placeholder="Post Body"
+                        value={postBody}
+                        onChangeText={setPostBody}
+                    />
+                    <Button
+                        title={isPosting ? 'Posting...' : 'Add Post'}
+                        onPress={addPost}
+                        disabled={isPosting}
+                    />
+                </View>
+                <View style={styles.listContainer}>
+                    <FlatList
+                        data={postList}
+                        renderItem={({ item }) => {
+                            return (
+                                <View style={styles.card}>
+                                    <Text style={styles.titleText}>
+                                        {item.title}
+                                    </Text>
+                                    <Text styles={styles.bodyText}>
+                                        {item.body}
+                                    </Text>
+                                </View>
+                            );
+                        }}
+                        ItemSeparatorComponent={() => {
+                            return <View style={{ height: 16 }} />;
+                        }}
+                        ListEmptyComponent={() => {
+                            return (
+                                <View style={styles.card}>
+                                    <Text>No data found</Text>
+                                </View>
+                            );
+                        }}
+                        ListHeaderComponent={() => {
+                            return (
+                                <Text style={styles.headerText}>Post List</Text>
+                            );
+                        }}
+                        ListFooterComponent={() => {
+                            return (
+                                <Text style={styles.footerText}>
+                                    Total: {postList.length}
                                 </Text>
-                                <Text styles={styles.bodyText}>
-                                    {item.body}
-                                </Text>
-                            </View>
-                        );
-                    }}
-                    ItemSeparatorComponent={() => {
-                        return <View style={{ height: 16 }} />;
-                    }}
-                    ListEmptyComponent={() => {
-                        return (
-                            <View style={styles.card}>
-                                <Text>No data found</Text>
-                            </View>
-                        );
-                    }}
-                    ListHeaderComponent={() => {
-                        return <Text style={styles.headerText}>Post List</Text>;
-                    }}
-                    ListFooterComponent={() => {
-                        return (
-                            <Text style={styles.footerText}>
-                                Total: {postList.length}
-                            </Text>
-                        );
-                    }}
-                    refreshing={refreshing}
-                    onRefresh={handleRefresh}
-                />
-            </View>
+                            );
+                        }}
+                        refreshing={refreshing}
+                        onRefresh={handleRefresh}
+                    />
+                </View>
+            </>
         </SafeAreaView>
     );
 }
@@ -128,5 +179,21 @@ const styles = StyleSheet.create({
         color: '#333',
         textAlign: 'center',
         marginTop: 16,
+    },
+    inputContainer: {
+        backgroundColor: '#fff',
+        padding: 16,
+        borderRadius: 8,
+        borderWidth: 1,
+        margin: 16,
+        width: '90%',
+    },
+    input: {
+        height: 40,
+        borderColor: 'gray',
+        borderWidth: 1,
+        marginBottom: 8,
+        padding: 8,
+        borderRadius: 8,
     },
 });
